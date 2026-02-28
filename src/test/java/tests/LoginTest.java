e015242fcc1e51f690a5227d6326b553d5241fd8
Denis <bloguserprogrammer1@gmail.com>
2026-02-28 22:46:44 +0300

    Version - 4.1 LoginTest modified

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -25,4 +25,48 @@ public class LoginTest extends BaseTest {
                 "Epic sadface: Sorry, this user has been locked out.",
                 "Не верный текст сообщения об ошибке");
     }
+
+    @Test
+    public void invalidUsername() {
+        loginPage.open();
+        loginPage.login("Standard_user", "secret_sauce");
+
+        assertTrue(loginPage.isErrorDisplayed(), "Нет сообщения об ошибке");
+        assertEquals(loginPage.getErrorText(),
+                "Epic sadface: Username and password do not match any user in this service",
+                "Не верный текст сообщения об ошибке");
+    }
+
+    @Test
+    public void emptyUser() {
+        loginPage.open();
+        loginPage.login("", "secret_sauce");
+
+        assertTrue(loginPage.isErrorDisplayed(), "Нет сообщения об ошибке");
+        assertEquals(loginPage.getErrorText(),
+                "Epic sadface: Username is required",
+                "Не верный текст сообщения об ошибке");
+    }
+
+    @Test
+    public void emptyPassword() {
+        loginPage.open();
+        loginPage.login("Standard_user", "");
+
+        assertTrue(loginPage.isErrorDisplayed(), "Нет сообщения об ошибке");
+        assertEquals(loginPage.getErrorText(),
+                "Epic sadface: Password is required",
+                "Не верный текст сообщения об ошибке");
+    }
+
+    @Test
+    public void emptyFields() {
+        loginPage.open();
+        loginPage.login("", "");
+
+        assertTrue(loginPage.isErrorDisplayed(), "Нет сообщения об ошибке");
+        assertEquals(loginPage.getErrorText(),
+                "Epic sadface: Username is required",
+                "Не верный текст сообщения об ошибке");
+    }
 }
